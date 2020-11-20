85fd1b886d062a529a7a1e868bc7c5224290416d
荒木　信哉 <shinnya@MacBook-Pro.local>
2020-11-20 18:14:58 +0900

    [finish]

---
+++ src/Rectangle.java
@@ -0,0 +1,15 @@
+
+public class Rectangle {
+	int width;
+	int height;
+
+	public Rectangle(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + width + "," + height + "]";
+	}
+}
